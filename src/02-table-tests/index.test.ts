516f501212dc275e17f4350e239e50f10499b23e
Aliaksandr Sekunau <alexandrsekunov@gmail.com>
2025-11-10 00:30:18 +0300

    feat: add basic tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,65 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  // Add
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  // Subtract
+  { a: 3, b: 1, action: Action.Subtract, expected: 2 },
+  { a: 1, b: -2, action: Action.Subtract, expected: 3 },
+  { a: 2, b: 0, action: Action.Subtract, expected: 2 },
+  // Multiply
+  { a: 2, b: 3, action: Action.Multiply, expected: 6 },
+  { a: 0, b: 2, action: Action.Multiply, expected: 0 },
+  { a: 2, b: 0, action: Action.Multiply, expected: 0 },
+  { a: 2, b: -2, action: Action.Multiply, expected: -4 },
+  // Divide
+  { a: 6, b: 2, action: Action.Divide, expected: 3 },
+  { a: 6, b: 0, action: Action.Divide, expected: Infinity },
+  { a: 6, b: -2, action: Action.Divide, expected: -3 },
+  { a: 1, b: 3, action: Action.Divide, expected: 1 / 3 },
+  { a: 1, b: 5, action: Action.Divide, expected: 0.2 },
+  // Exponentiate
+  { a: 2, b: 4, action: Action.Exponentiate, expected: 16 },
+  { a: 2, b: 1, action: Action.Exponentiate, expected: 2 },
+  { a: 2, b: 0, action: Action.Exponentiate, expected: 1 },
+  { a: 0, b: 2, action: Action.Exponentiate, expected: 0 },
+  { a: 2, b: -2, action: Action.Exponentiate, expected: 0.25 },
+  // Invalid action
+  { a: 2, b: 1, action: 'divide', expected: null },
+  { a: 2, b: 1, action: 'abc', expected: null },
+  // Invalid arguments
+  { a: 1, b: 'b', action: Action.Add, expected: null },
+  { a: 1, b: null, action: Action.Add, expected: null },
+  { a: undefined, b: 2, action: Action.Add, expected: null },
+  { a: 3, b: [], action: Action.Add, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  test.each(testCases)(
+    'should return $expected for case $a $b and action "$action"',
+    ({ a, b, action, expected }) => {
+      expect(
+        simpleCalculator({
+          a: a,
+          b: b,
+          action: action,
+        }),
+      ).toBe(expected);
+    },
+  );
+
+  // alternative solution with loop
+  // testCases.forEach((item, index) => {
+  //   test(`should return ${item.expected} for case ${index}`, () => {
+  //     expect(
+  //       simpleCalculator({
+  //         a: item.a,
+  //         b: item.b,
+  //         action: item.action,
+  //       }),
+  //     ).toBe(item.expected);
+  //   });
+  // });
 });
--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,44 +1,115 @@
-// Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import { getBankAccount } from '.';
+import lodash from 'lodash';
+
+let randomMock: jest.SpyInstance;
+
+const initialBalance = 1000;
 
 describe('BankAccount', () => {
+  let bankAccount = getBankAccount(initialBalance);
+  let anotherBankAccount = getBankAccount(initialBalance);
+
+  beforeEach(() => {
+    bankAccount = getBankAccount(initialBalance);
+    anotherBankAccount = getBankAccount(initialBalance);
+
+    randomMock = jest.spyOn(lodash, 'random');
+  });
+
+  afterEach(() => {
+    randomMock.mockRestore();
+  });
+
   test('should create account with initial balance', () => {
-    // Write your test here
+    expect(bankAccount).toEqual({ _balance: initialBalance });
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    const withdrawAmount = 1001;
+
+    expect(() => bankAccount.withdraw(withdrawAmount)).toThrow(
+      `Insufficient funds: cannot withdraw more than ${initialBalance}`,
+    );
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    const transferAmount = 1001;
+
+    expect(() =>
+      bankAccount.transfer(transferAmount, anotherBankAccount),
+    ).toThrow(
+      `Insufficient funds: cannot withdraw more than ${initialBalance}`,
+    );
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    const transferAmount = 1001;
+
+    expect(() => bankAccount.transfer(transferAmount, bankAccount)).toThrow(
+      'Transfer failed',
+    );
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    const depositAmount = 100;
+    bankAccount.deposit(depositAmount);
+    const expectedBalance = initialBalance + depositAmount;
+
+    expect(bankAccount.getBalance()).toBe(expectedBalance);
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    const withdrawAmount = 100;
+    bankAccount.withdraw(withdrawAmount);
+    const expectedBalance = initialBalance - withdrawAmount;
+
+    expect(bankAccount.getBalance()).toBe(expectedBalance);
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    const transferAmount = 100;
+    bankAccount.transfer(transferAmount, anotherBankAccount);
+    const expectedBankAccountBalance = initialBalance - transferAmount;
+    const expectedAnotherBankAccountBalance = initialBalance + transferAmount;
+
+    expect(bankAccount.getBalance()).toBe(expectedBankAccountBalance);
+    expect(anotherBankAccount.getBalance()).toBe(
+      expectedAnotherBankAccountBalance,
+    );
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
-    // Write your tests here
+    const randomBalance = 1;
+    const randomRequest = 1;
+    randomMock
+      .mockReturnValueOnce(randomBalance)
+      .mockReturnValueOnce(randomRequest);
+    const expectedBalance = await bankAccount.fetchBalance();
+
+    expect(expectedBalance).toBe(randomBalance);
   });
 
   test('should set new balance if fetchBalance returned number', async () => {
-    // Write your tests here
+    const randomBalance = 100;
+    const randomRequest = 1;
+    randomMock
+      .mockReturnValueOnce(randomBalance)
+      .mockReturnValueOnce(randomRequest);
+    await bankAccount.synchronizeBalance();
+    const expectedBalance = bankAccount.getBalance();
+
+    expect(expectedBalance).toBe(randomBalance);
   });
 
   test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
-    // Write your tests here
+    const randomBalance = 1;
+    const randomRequest = 0;
+    randomMock
+      .mockReturnValueOnce(randomBalance)
+      .mockReturnValueOnce(randomRequest);
+
+    await expect(bankAccount.synchronizeBalance()).rejects.toThrow(
+      'Synchronization failed',
+    );
   });
 });
